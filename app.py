ff87aae611a8fc542ac9aeadbd6bff6dd9f7fed2
Yejin <shiny20@myunitec.ac.nz>
2026-05-30 23:01:38 +1200

    Health check endpoint has been addded

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 import flask
+from flask import jsonify
 
 app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
     return 'API works!'
+
+@app.route("/health")
+def health():
+    return jsonify(status="ok")
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host='0.0.0.0')
